36e8501ede988debde854bff5b901dc6f66cc043
chieyine <chieyinenelson@gmail>
2026-07-24 23:07:50 +0100

    fix: scope eslint rules to correct file extensions for plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextConfig from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...nextConfig,
   {
+    files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "react/no-unescaped-entities": "off"
     }
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.mjs", "**/*.cjs"],
+    rules: {
+      "react/no-unescaped-entities": "off"
+    }
   }
 ];
-
-export default eslintConfig;
